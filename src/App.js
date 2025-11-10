d34f660ef8896b445880d3eeb5dedbee09ffcae4
asgharmehmood5533-max <asgharmehmood5533@gmail.com>
2025-11-10 12:31:46 +0500

    i created a a new respository with the name of my app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+function App(){
+  return(
+    <div className='App'>
+      <div>
+        <pre><h1>hello world       hello world      hello world</h1></pre>
+       <br>
+       </br>
+
+      
+      </div>
+      <div className='App2'>
+        <pre><h1>hello world       hello world      hello world</h1></pre>
+      </div>
+       <br>
+       </br> 
+      <div className='App3'>
+       <pre><h1>hello world          hello world     hello world</h1></pre>
+      </div>
+    </div>
+  );
+}
+
+export default App;
