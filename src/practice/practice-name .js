ade0bdf89c12e26c755b80c85260812fedd10788
asgharmehmood5533-max <asgharmehmood5533@gmail.com>
2025-11-10 19:37:02 +0500

    MY CHANGES

---
+++ src/practice/practice-name .js
@@ -0,0 +1,16 @@
+export const Named_Component = () => {
+    return (
+        <div>
+            <h1>This is a named exported component!</h1>
+            <p>This is a paragraph inside the named exported component.</p>
+        </div>
+    );
+};
+export const Another_Named_Component = () => {
+    return (
+        <div>
+            <h2>This is another named exported component!</h2>
+            <p>This is a paragraph inside another named exported component.</p>
+        </div>
+    );          
+};
---
+++ src/practice/practice_default.js
@@ -0,0 +1,7 @@
+import React from "react";  
+const Default_Component = () => {  
+    return <div><h1>This is the default exported component!</h1>
+    <p>This is a paragraph inside the default exported component.</p>
+    </div>;  
+}   
+export default Default_Component;
